8ad83702a9fbba8006544aeba8e183593ac997c5
Melissa Lonsdale <melissa.lonsdale@gmail.com>
2018-09-11 16:22:02 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,28 @@
 //define functions here
+function getIt() {
+  $("p").on("click")
+  alert("Hey!");
+  return;
+}
+
+
+function frameIt() {
+  $("img").on("load").addClass("tasty");
+  return;
+}
+
+function pressIt() {
+  $(document).on('keydown', function(key) {
+    if(key.which == 71){
+        alert('s was pressed');
+    }
+  });
+}
+
+function submitIt() {
+  $("form input:first").on("submit");
+  alert('Your form is going to be submitted now.');
+}
 
 $(document).ready(function(){
 
